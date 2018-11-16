31299d3117f12cfacc0703e153a047f5605e2032
Sean Robertson <sdrobert@cs.toronto.edu>
2018-11-16 17:50:28 -0500

    Fixing up setup.py classifiers

--- setup.py
+++ setup.py
@@ -36,15 +36,14 @@ def main():
         packages=['pydrobert', 'pydrobert.torch'],
         classifiers=[
             'Development Status :: 3 - Alpha',
-            'Intended Audience :: Researchers',
-            'Programming Language :: Python',
+            'License :: OSI Approved :: Apache Software License',
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7'
+            'Programming Language :: Python :: 3.7',
         ],
         install_requires=[
             'numpy', 'future', 'torch', 'param'
