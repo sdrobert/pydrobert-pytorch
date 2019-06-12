6b2dff85372b741f43656316513e60e6e8fb3d89
Sean Robertson <sdrobert@cs.toronto.edu>
2019-06-12 17:37:49 -0400

    Some boilerplate

---
+++ tests/test_estimators.py
@@ -0,0 +1,12 @@
+__author__ = "Sean Robertson"
+__email__ = "sdrobert@cs.toronto.edu"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2019 Sean Robertson"
+
+import pytest
+import pydrobert.torch.estimators as estimators
+
+
+@pytest.mark.cpu
+def test_dummy():
+    pass
