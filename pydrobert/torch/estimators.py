bcdbf62fda8490fd5892b0694cfdeaeb392556e0
Sean Robertson <sdrobert@cs.toronto.edu>
2019-06-13 17:45:09 -0400

    Sampling, tests

--- pydrobert/torch/estimators.py
+++ pydrobert/torch/estimators.py
@@ -12,7 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-'''Gradient estimators'''
+r'''Gradient estimators
+
+Much of this code has been adapted from `David Duvenaud's repo
+<https://github.com/duvenaud/relax>`_.
+
+The goal of this module is to find some estimate
+
+.. math:: g \approx d fb / d \theta
+
+where `fb` is assumed not to be differentiable with respect to :math:`\theta`
+as it relies on some :math:`b \sim \theta`. Instead of :math:`\theta`, we use
+`logits`, where :math:`logits = \log(\theta / (1 - \theta))` when `\theta`
+parameterizes Bernoullis in the usual way, and :math:`logits = \log(\theta)`
+when `\theta` parameterizes categorical distributions in the usual way. `g` can
+be plugged into backpropagation via something like ``logits.backward(g)``.
+In this way, `logits` can be the output of a neural layer.
+
+Different estimators require different arguments. In general, you'll need to
+know what distribution you're parameterizing, the tensor containing the
+parametrization, and have the function definition `f`. There are a number of
+utility functions in this module with the template ``to_x`` that can convert
+between these as necessary. This isn't done automatically, because `logits`,
+`b`, and `fb` are not always available all at once.
+'''
 
 from __future__ import absolute_import
 from __future__ import division
@@ -24,3 +47,52 @@ __author__ = "Sean Robertson"
 __email__ = "sdrobert@cs.toronto.edu"
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2019 Sean Robertson"
+
+
+def to_z(logits, dist):
+    '''Samples random noise, then injects it into logits to produce z
+
+    Parameters
+    ----------
+    logits : torch.FloatTensor
+    dist : {"bern", "cat"}
+
+    Returns
+    -------
+    z : torch.FloatTensor
+    '''
+    u = torch.distributions.utils.clamp_probs(torch.rand_like(logits))
+    if dist in {"bern", "Bern", "bernoulli", "Bernoulli"}:
+        z = logits.detach() + torch.log(u) - torch.log1p(-u)
+    elif dist in {"cat", "Cat", "categorical", "Categorical"}:
+        z = logits.detach() - torch.log(-torch.log(u))
+    else:
+        raise ValueError("Unknown distribution {}".format(dist))
+    z.requires_grad_(logits.requires_grad)
+    return z
+
+
+def to_b(z, dist):
+    '''Converts z to sample using a deterministic mapping
+
+    Parameters
+    ----------
+    z : torch.FloatTensor
+    dist : {"bern", "cat"}
+
+    Returns
+    -------
+    b : torch.FloatTensor
+    '''
+    if dist in {"bern", "Bern", "bernoulli", "Bernoulli"}:
+        b = z.gt(0.)
+    elif dist in {"cat", "Cat", "categorical", "Categorical"}:
+        b = z.argmax(dim=-1)
+    else:
+        raise ValueError("Unknown distribution {}".format(dist))
+    return b
+
+
+def to_fb(b, f):
+    '''Simply call f(b)'''
+    return f(b)
--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -3,10 +3,53 @@ __email__ = "sdrobert@cs.toronto.edu"
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2019 Sean Robertson"
 
+from itertools import product
+
+import torch
 import pytest
 import pydrobert.torch.estimators as estimators
 
 
+def _expectation(f, logits, dist):
+    if dist == "bern":
+        theta = torch.sigmoid(logits)
+        pop = (
+            torch.LongTensor(b).view_as(logits)
+            for b in product(range(2), repeat=logits.nelement())
+        )
+        return sum(
+            f(b).to(theta) *
+            (b.to(theta) * theta + (1 - b.to(theta)) * (1 - theta)).prod()
+            for b in pop
+        )
+    else:
+        pop = (
+            torch.LongTensor(b).view(logits.shape[:-1])
+            for b in product(
+                range(logits.shape[-1]),
+                repeat=logits.nelement() // logits.shape[-1])
+        )
+        return sum(
+            f(b).to(logits) * (-torch.nn.functional.nll_loss(
+                logits.log_softmax(-1).view(-1, logits.shape[-1]),
+                b.flatten(), reduction='sum')).exp()
+            for b in pop
+        )
+
+
 @pytest.mark.cpu
-def test_dummy():
-    pass
+@pytest.mark.parametrize("dist", ["bern", "cat"])
+@pytest.mark.parametrize("seed", [1, 2, 3])
+def test_z(dist, seed):
+    torch.manual_seed(seed)
+    theta = torch.distributions.utils.clamp_probs(torch.rand(2, 2, 4))
+    if dist == "bern":
+        logits = torch.log(theta) - torch.log1p(-theta)
+    else:
+        logits = torch.log(theta)
+    exp = _expectation(lambda b: b, logits, dist)
+    logits = logits[None, ...].expand((1000000, ) + logits.shape)
+    b = estimators.to_b(estimators.to_z(logits, dist), dist)
+    act = b.to(theta).mean(0)
+    assert exp.shape == act.shape
+    assert torch.allclose(exp, act, atol=1e-1)
