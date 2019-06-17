Fixed up for surrogate backprop; test

pydrobert/torch/estimators.py
@@ -191,7 +191,7 @@ def _to_z_tilde(logits, b, dist):
     return z_tilde
 
 
-def relax(fb, b, logits, z, surrogate, dist):
+def relax(fb, b, logits, z, surrogate, dist, components=False):
     r'''Perform RELAX gradient estimation
 
     RELAX has a single-sample implementation as
@@ -208,6 +208,14 @@ def relax(fb, b, logits, z, surrogate, dist):
     It is an unbiased estimate of the derivative of the expectation w.r.t
     `logits`.
 
+    `g` is itself differentiable with respect to the parameters of the
+    surrogate. If the surrogate is trainable, an easy choice for its loss is to
+    minimize the variance of `g` via ``(g ** 2).sum().backward()``. Propagating
+    directly from `g` should be suitable for most situations. Insofar as the
+    loss cannot be directly computed from `g`, setting the argument for
+    `components` to true will return a tuple containing the terms of `g`
+    instead.
+
     Parameters
     ----------
     fb : torch.Tensor
@@ -219,10 +227,16 @@ def relax(fb, b, logits, z, surrogate, dist):
         a tensor of the same shape if modelling a Bernoulli, or of shape
         ``z[..., 0]`` (minus the last dimension) if Categorical.
     dist : {"bern", "cat"}
+    components : bool, optional
 
     Returns
     -------
-    g : torch.Tensor
+    g : torch.Tensor or tuple
+        If `components` is ``False``, `g` will be the gradient estimate with
+        respect to `logits`. Otherwise, a tuple will be returned of
+        ``(diff, dlog_pb, dc_z, dc_z_tilde)`` which correspond to the terms
+        in the above equation and can reconstruct `g` as
+        ``g = diff * dlog_pb + dc_z - dc_z_tilde``.
     '''
     fb = fb.detach()
     b = b.detach()
@@ -240,9 +254,15 @@ def relax(fb, b, logits, z, surrogate, dist):
         raise ValueError("Unknown distribution {}".format(dist))
     ones = torch.ones_like(c_z)
     dlog_pb, = torch.autograd.grad([log_pb], [logits], grad_outputs=ones)
+    # we need `create_graph` to be true here or backpropagation through the
+    # surrogate will not include the derivative terms except as scalar values
     dc_z, = torch.autograd.grad(
-        [c_z], [logits], retain_graph=True, grad_outputs=ones)
+        [c_z], [logits],
+        create_graph=True, retain_graph=True, grad_outputs=ones)
     dc_z_tilde, = torch.autograd.grad(
-        [c_z_tilde], [logits], retain_graph=True, grad_outputs=ones)
-    g = diff * dlog_pb + dc_z - dc_z_tilde
-    return g
+        [c_z_tilde], [logits],
+        create_graph=True, retain_graph=True, grad_outputs=ones)
+    if components:
+        return (diff, dlog_pb, dc_z, dc_z_tilde)
+    else:
+        return diff * dlog_pb + dc_z - dc_z_tilde

tests/test_estimators.py
@@ -45,8 +45,26 @@ def test_z(dist, seed):
     assert torch.allclose(exp, act, atol=1e-1)
 
 
+class Surrogate(torch.nn.Module):
+
+    def __init__(self, dist):
+        super(Surrogate, self).__init__()
+        self.dist = dist
+        self.weight = torch.nn.Parameter(torch.Tensor(1))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.weight.data.uniform_(-1., 1.)
+
+    def forward(self, inp):
+        outp = inp * self.weight
+        if self.dist == "cat":
+            outp = outp.sum(-1)
+        return outp
+
+
 @pytest.mark.cpu
-@pytest.mark.parametrize("seed", [1, 2, 3])
+@pytest.mark.parametrize("seed", [4, 5, 6])
 @pytest.mark.parametrize("dist", ["bern", "cat"])
 @pytest.mark.parametrize("est", ["reinforce", "relax"])
 @pytest.mark.parametrize("objective", [
@@ -58,25 +76,49 @@ def test_z(dist, seed):
 ])
 def test_bias(seed, dist, est, objective):
     torch.manual_seed(seed)
-    logits = torch.randn(2, 4)
-    logits.requires_grad_(True)
+    logits = torch.randn(2, 4, requires_grad=True)
     exp = _expectation(objective, logits, dist)
     exp, = torch.autograd.grad(
         [exp], [logits], grad_outputs=torch.ones_like(exp))
-    logits = logits[None, ...].expand((10000,) + logits.shape)
+    # if these tests fail, the number of markov samples might be too low. If
+    # you keep raising this but it appears unable to meet the tolerance,
+    # it's probably bias
+    logits = logits[None, ...].expand((30000,) + logits.shape)
     z = estimators.to_z(logits, dist)
     b = estimators.to_b(z, dist)
     fb = estimators.to_fb(objective, b)
     if est == 'reinforce':
         g = estimators.reinforce(fb, b, logits, dist)
     elif est == "relax":
-
-        def surrogate(z):
-            if dist == "cat":
-                return z.sum(-1)
-            else:
-                return z
-        g = estimators.relax(fb, b, logits, z, surrogate, dist)
+        g = estimators.relax(fb, b, logits, z, Surrogate(dist), dist)
     g = g.mean(0)
     assert exp.shape == g.shape
     assert torch.allclose(exp, g, atol=1e-1)
+
+
+@pytest.mark.cpu
+@pytest.mark.parametrize("dist", ["bern", "cat"])
+def test_relax_surrogate_backprop(dist):
+    torch.manual_seed(1)
+    logits = torch.randn(10, 5, 4, requires_grad=True)
+    z = estimators.to_z(logits, dist)
+    b = estimators.to_b(z, dist)
+    fb = torch.rand_like(b)
+    surrogate = Surrogate(dist)
+    diff, dlog_pb, dc_z, dc_z_tilde = estimators.relax(
+        fb, b, logits, z, surrogate, dist, components=True)
+    torch.autograd.grad(
+        [diff], [logits], retain_graph=True,
+        grad_outputs=torch.ones_like(diff))
+    # for bernoulli, grad is
+    torch.autograd.grad(
+        [dc_z], [logits], retain_graph=True,
+        grad_outputs=torch.ones_like(dc_z),
+        allow_unused=True,
+    )
+    torch.autograd.grad(
+        [dc_z_tilde], [logits], retain_graph=True,
+        grad_outputs=torch.ones_like(dc_z_tilde))
+    g = diff * dlog_pb + dc_z - dc_z_tilde
+    (g ** 2).sum().backward()
+    surrogate.weight.grad