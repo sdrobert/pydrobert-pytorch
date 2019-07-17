0e215520f6d2058496eb2c3e888ed073ed49d34f
Sean Robertson <sdrobert@cs.toronto.edu>
2019-07-17 19:28:30 -0400

    Added beam_search_advance, tests

--- pydrobert/torch/util.py
+++ pydrobert/torch/util.py
@@ -25,8 +25,168 @@ __email__ = "sdrobert@cs.toronto.edu"
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2019 Sean Robertson"
 __all__ = [
+    'beam_search_advance',
     'optimizer_to',
 ]
+
+
+def beam_search_advance(logits, width, log_prior=None, y_prev=None, eos=None):
+    r'''Advance a beam search
+
+    Suppose a model outputs a un-normalized log-probability distribution over
+    the next element of a sequence in `logits` s.t.
+
+    .. math::
+
+        Pr(y_t = c) = exp(logits_c) / \sum_k exp(logits_k)
+
+    `logits` is "auto-regressive" if it is conditioned upon a prefix sequence
+    :math:`y_{<t}`. If `logits` is not auto-regressive, beam search (this
+    function) is not needed, since `logits` will produce the same probability
+    distribution over :math:`y_t` regardless of what it's predicted up to that
+    point. So we assume from now on that :math:`logits = f(y_{<t})`.
+    Critically, we assume that `logits` is conditionally independent of any
+    `logits` from prior timesteps given :math:`y_{<t}`
+
+    Beam search is a heuristic mechanism for determining a best path, i.e.
+    :math:`\argmax_y Pr(y)` that maximizes the probability of the best path
+    by keeping track of `width` high probability paths called "beams"
+    (the aggregate of which for a given time step is named, unfortunately,
+    "the beam").
+
+    This function is called at every time step. It updates old beam
+    log-probabilities (`log_prior`) with new ones (`score`), and updates
+    us the class indices emitted between them (`y`). See the examples section
+    for how this might work.
+
+    Parameters
+    ----------
+    logits : torch.tensor
+        The conditional probabilities over class labels for the current tim
+        step. Either of shape ``(num_batches, old_width, num_classes)``,
+        where ``old_width`` is the number of beams in the previous time step,
+        or ``(num_batches, num_classes)``, where it is assumed that
+        ``old_width == 1``
+    width : int
+        The number of beams in the beam to produce for the current time step.
+        ``width <= num_classes``
+    log_prior : torch.tensor, optional
+        A tensor of (or proportional to) log prior probabilities of beams up
+        to the previous time step. Either of shape ``(num_batches, old_width)``
+        or ``(num_batches,)``. If unspecified, a uniform log prior will be used
+    y_prev : torch.LongTensor, optional
+        A tensor of shape ``(t - 1, num_batches, old_width)`` or
+        ``(t - 1, num_batches)`` specifying :math:`y_{<t}`. If unspecified,
+        it is assumed that ``t == 1``
+    eos : int, optional
+        If both `eos` and `y_prev` are specified, whenever
+        ``y_prev[-1, i, j] == eos``, the indexed beam is considered "finished"
+        and will not update its value with `logits`. `y` will copy the `eos`
+        symbol into :math:`y_t`.
+
+
+    Returns
+    -------
+    score, y, s : torch.tensor, torch.LongTensor, torch.LongTensor
+        `score` is a tensor of shape ``(num_batches, width)`` of the
+        log-joint probabilities of the new beams in the beam. `y` is a
+        tensor of shape ``(t, num_batches, width)`` of indices of the class
+        labels generated up to this point. `s` is a tensor of shape
+        ``(num_batches, width)`` of indices of beams in the old beam which
+        prefix the new beam. Note that beams in the new beam are sorted by
+        descending probability
+
+    Examples
+    --------
+
+    Decoding with beam search, assuming index class 0 is eos and -1 is start
+
+    >>> N, I, C, T, W, H = 5, 5, 10, 100, 5, 10
+    >>> cell = torch.nn.RNNCell(I + 1, H)
+    >>> ff = torch.nn.Linear(H, C)
+    >>> inp = torch.rand(T, N, I)
+    >>> y = torch.full((1, N, 1), -1, dtype=torch.long)
+    >>> h_t = torch.zeros(N, 1, H)
+    >>> score = None
+    >>> for inp_t in inp:
+    >>>     y_tm1 = y[-1]
+    >>>     old_width = y_tm1.shape[-1]
+    >>>     inp_t = inp_t.unsqueeze(1).expand(N, old_width, I)
+    >>>     x_t = torch.cat([inp_t, y_tm1.unsqueeze(2).float()], -1)
+    >>>     h_t = cell(
+    ...         x_t.view(N * old_width, I + 1),
+    ...         h_t.view(N * old_width, H)
+    ...     ).view(N, old_width, H)
+    >>>     logits_t = ff(h_t)
+    >>>     score, y, s_t = beam_search_advance(logits_t, W, score, y, 0)
+    >>>     h_t = h_t.gather(1, s_t.unsqueeze(-1).expand(N, W, H))
+    >>> bests = []
+    >>> for batch_idx in range(N):
+    >>>     best_beam_path = y[1:, batch_idx, 0]
+    >>>     not_special_mask = best_beam_path.ne(0)
+    >>>     best_beam_path = best_beam_path.masked_select(not_special_mask)
+    >>>     bests.append(best_beam_path)
+    '''
+    if logits.dim() == 2:
+        logits = logits.unsqueeze(1)
+    elif logits.dim() != 3:
+        raise ValueError('logits must have dimension of either 2 or 3')
+    num_batches, old_width, num_classes = logits.shape
+    if log_prior is None:
+        log_prior = torch.full(
+            (num_batches, old_width),
+            -torch.log(torch.tensor(float(num_classes))),
+            dtype=logits.dtype, device=logits.device,
+        )
+    elif tuple(log_prior.shape) == (num_batches,) and old_width == 1:
+        log_prior = log_prior.unsqueeze(1)
+    elif log_prior.shape != logits.shape[:-1]:
+        raise ValueError(
+            'If logits of shape {} then log_prior must have shape {}'.format(
+                (num_batches, old_width, num_classes),
+                (num_batches, old_width),
+            ))
+    if y_prev is not None and y_prev.shape[1:] != log_prior.shape:
+        raise ValueError(
+            'If logits of shape {} then y_prev must have shape (*, {}, {})'
+            ''.format(
+                (num_batches, old_width, num_classes),
+                num_batches, old_width,
+            )
+        )
+    logits = torch.nn.functional.log_softmax(logits, 2)
+    if y_prev is not None and eos is not None:
+        if eos < 0:
+            raise ValueError('eos must be a valid index')
+        done_mask = y_prev[-1].eq(eos)
+        num_done = done_mask.sum().item()
+        if num_done:
+            if num_done == old_width and old_width < width:
+                warnings.warn(
+                    'New beam width ({}) is wider than old beam width ({}), '
+                    'but all paths are already done. Reducing new width.'
+                    ''.format(width, old_width))
+                width = num_done
+            # we want finished beams to only ever be in the top k when the next
+            # class in the beam is EOS, so we fill all the class labels of old
+            # beams with -inf except EOS, which gets a 0
+            done_classes = torch.full_like(logits[0, 0], -float('inf'))
+            done_classes[eos] = 0.
+            logits = torch.where(
+                done_mask.unsqueeze(-1),
+                done_classes,
+                logits
+            )
+    joint = log_prior.unsqueeze(2) + logits
+    score, idxs = torch.topk(joint.view(num_batches, -1), width, dim=1)
+    s = idxs // num_classes
+    y = (idxs % num_classes).unsqueeze(0)
+    if y_prev is not None:
+        tm1 = y_prev.shape[0]
+        y_prev = y_prev.gather(
+            2, s.unsqueeze(0).expand(tm1, num_batches, width))
+        y = torch.cat([y_prev, y], 0)
+    return score, y, s
 
 
 def optimizer_to(optimizer, to):
---
+++ tests/test_util.py
@@ -0,0 +1,115 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import pytest
+import pydrobert.torch.util as util
+
+__author__ = "Sean Robertson"
+__email__ = "sdrobert@cs.toronto.edu"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2019 Sean Robertson"
+
+
+@pytest.mark.cpu
+def test_beam_search_advance_greedy():
+    torch.manual_seed(50)
+    N, C, T = 30, 100, 25
+    logits = torch.randn(T, N, C)
+    greedy_logits, greedy_paths = torch.nn.functional.log_softmax(
+        logits, -1).max(2)
+    greedy_scores = greedy_logits.sum(0) + torch.log(torch.tensor(1 / C))
+    y = None
+    score = None
+    for logit in logits:
+        score, y, _ = util.beam_search_advance(logit, 1, score, y)
+    score, y = score.squeeze(1), y.squeeze(2)
+    assert torch.allclose(score, greedy_scores)
+    assert torch.all(y == greedy_paths)
+
+
+def test_beam_search_advance_steps(device):
+    logits_1 = torch.tensor([
+        [-10, -2, -4, -10],
+        [-2, 0, 1, 1.1],
+    ], device=device)  # ~[[x, 0, -4, x], [x, x, -.8, -.6]]
+    score_1, y_1, s_1 = util.beam_search_advance(logits_1, 2)
+    assert torch.all(y_1 == torch.tensor([[
+        [1, 2],
+        [3, 2],
+    ]], device=device))
+    assert torch.all(s_1 == 0)
+    logits_2 = torch.tensor([
+        [  # beam for batch 1
+            [-5., -6, -7, -8.],
+            [-400, -300, -200, -1],
+        ],
+        [  # beam for batch 2
+            [2, 1, 1, 1],
+            [-1, -2, -3, -4],
+        ],
+    ], device=device)  # ~ [[[-.4 -1.4 x x] [x x x 0]],
+    #                      [[-.7 -1.7 x x] [-.4 -1.4 x x]]
+    # batch 0: 0->0 0->1 win b/c 1->3 can't make up for score_1
+    # batch 1: 1->0 0->0 win b/c score_1 about even
+    score_2, y_2, s_2 = util.beam_search_advance(logits_2, 2, score_1)
+    assert torch.all(y_2 == torch.tensor([[
+        [0, 1],
+        [0, 0],
+    ]], device=device))
+    assert torch.all(s_2 == torch.tensor([
+        [0, 0],
+        [1, 0],
+    ], device=device))
+    logits_3 = torch.tensor([
+        [
+            [1000., 0, 0, 0],
+            [0, -100, 0, 0],
+        ],
+        [
+            [0, 0, 0, 100],
+            [2, 2, 1000, 10],
+        ],
+    ], device=device)  # ~ [[[0 x x x] [-1 -101 -1 -1]],
+    #                      [[x x x 0] [x x 0 x]]
+    # batch 0: 0->0 1->1 batch 1 done, but no priority b/c 0->0 very small
+    # batch 1: 0->3 1->2
+    score_3, y_3, s_3 = util.beam_search_advance(logits_3, 2, score_2, y_2, 1)
+    assert torch.all(y_3 == torch.tensor([
+        [  # y_2
+            [0, 1],
+            [0, 0],
+        ],
+        [
+            [0, 1],
+            [3, 2],
+        ]
+    ], device=device))
+    assert torch.all(s_3 == torch.tensor([
+        [0, 1],
+        [0, 1],
+    ], device=device))
+    logits_4 = torch.tensor([
+        [
+            [1., 2, 3, 4],
+            [5, 6, 7, 8],
+        ],
+        [
+            [2, 2, 3, 2],
+            [5, 6, 7, 8],
+        ]
+    ], device=device, requires_grad=True)
+    # (note no eos condition)
+    score_4, y_4, s_4 = util.beam_search_advance(logits_4, 1, score_3)
+    assert torch.all(y_4.flatten() == torch.tensor([3, 3], device=device))
+    assert torch.all(s_4.flatten() == torch.tensor([0, 1], device=device))
+    g = torch.autograd.grad(
+        [score_4], [logits_4], grad_outputs=torch.ones_like(score_4))[0]
+    # we should only have a gradient for the chosen beam per batch
+    assert torch.allclose(g[0, 1, :], torch.zeros_like(g[0, 1, :]))
+    assert torch.allclose(g[1, 0, :], torch.zeros_like(g[1, 0, :]))
+    # all elements of a non-peaked softmax in the chosen beam should have a
+    # non-negligible gradient
+    assert abs(g[0, 0, 1].item()) > 1e-4
+    assert abs(g[1, 1, 1].item()) > 1e-4
