0f699031a235e6e344beded1dff44d7f7c4ccdf8
sdrobert <sdrobert@cs.toronto.edu>
2022-02-14 13:28:06 -0500

    Hmm

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,6 +28,8 @@ import pydrobert.torch._compat as compat
 
 try:
     torch._C._jit_clear_class_registry()
+    torch.jit._recursive.concrete_type_store = torch.jit._recursive.ConcreteTypeStore()
+    torch.jit._state._clear_class_state()
 except:
     pass
 
