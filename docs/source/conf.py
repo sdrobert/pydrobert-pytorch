9a3b5a95753e1da3869c78ec2e2345ac475eb682
Sean Robertson <sdrobert@cs.toronto.edu>
2022-03-30 16:46:25 -0400

    Dude?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,13 +34,13 @@ language = "en"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
-    "myst_parser",
 ]
 
 napoleon_numpy_docstring = True
