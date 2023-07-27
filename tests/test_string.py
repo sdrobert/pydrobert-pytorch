699e7baf2ae0152748ed7ab613e2ce854c82f954
Sean Robertson <sdrobert@cs.toronto.edu>
2023-07-27 17:54:41 -0400

    Same effect without the trace?

--- tests/test_string.py
+++ tests/test_string.py
@@ -358,14 +358,14 @@ def test_hard_optimal_completion_distillation_loss_batch(device, jit_type):
     if jit_type == "script":
         loss = torch.jit.script(loss)
     elif jit_type == "trace":
-        loss = torch.jit.trace(
-            loss,
-            (
-                torch.empty(1, 1, C + 1),
-                torch.full((1, 1,), C, dtype=torch.long),
-                torch.full((1, 1), C, dtype=torch.long),
-            ),
-        )
+        # loss = torch.jit.trace(
+        #     loss,
+        #     (
+        #         torch.empty(1, 1, C + 1),
+        #         torch.full((1, 1,), C, dtype=torch.long),
+        #         torch.full((1, 1), C, dtype=torch.long),
+        #     ),
+        # )
         print("trace done")
     ref, hyp, logits = [], [], []
     l1_exp = torch.zeros(1, device=device)
